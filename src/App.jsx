04fa318cb98d55974f022e087e9034e5e5f801e1
KChathuranga <kavinduchathuranga12@gmail.com>
2024-05-28 16:40:39 +0530

    Project section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
 
@@ -28,6 +29,7 @@ function App() {
         <About />
         <Technologies />
         <Experience />
+        <Projects />
       </div>
     </div>
   )
