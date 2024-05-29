ef716eb0cebe6091bb62c7e9f692a1f4eedbbdd9
KChathuranga <kavinduchathuranga12@gmail.com>
2024-05-29 21:34:10 +0530

    animate contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,20 @@
 import { CONTACT } from "../constants"
+import { motion } from "framer-motion"
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-      <h2 className="my-10 text-center text-4xl">Contact Me</h2>
+      <motion.h2 
+        whileInView={{y: 0, opacity: 1}}
+        initial={{y: -100, opacity: 0}}
+        transition={{duration: 0.5}}
+        className="my-10 text-center text-4xl">Contact Me</motion.h2>
       <div className="text-center tracking-tighter">
-        <p className="my-4">{CONTACT.phoneNo}</p>
+        <motion.p 
+          whileInView={{x: 0, opacity: 1}}
+          initial={{x: -100, opacity: 0}}
+          transition={{duration: 1}}
+          className="my-4">{CONTACT.phoneNo}</motion.p>
         <a href="#" className="border-b ">{CONTACT.email}</a>
       </div>
     </div>
