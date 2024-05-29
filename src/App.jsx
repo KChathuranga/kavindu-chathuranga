c5c9bd221797acc16f322eb83e9d65978339a09e
KChathuranga <kavinduchathuranga12@gmail.com>
2024-05-29 21:51:38 +0530

    Commented the experience section

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <Hero />
         <About />
         <Technologies />
-        <Experience />
+        {/*<Experience />*/}
         <Projects />
         <Contact />
       </div>
